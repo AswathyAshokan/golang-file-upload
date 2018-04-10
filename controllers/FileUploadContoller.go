d8f90142ccedb7a6bfdcfb384604057bfa589a71
aswathy <aswathyashok85@gmail.com>
2018-04-10 13:18:13 +0530

    golang file upload

--- controllers/FileUploadContoller.go
+++ controllers/FileUploadContoller.go
@@ -16,6 +16,7 @@ import (
 	"image/jpeg"
 	"github.com/nfnt/resize"
 	"image/gif"
+
 )
 
 type FileUploadController struct {
@@ -25,196 +26,228 @@ type FileUploadController struct {
 func (c *FileUploadController)FileUpload(){
 	r := c.Ctx.Request
 	w := c.Ctx.ResponseWriter
-	type Config struct {
-		Database struct {
-			Picture     string `json:"picture"`
+	msec := strconv.FormatInt(time.Now().UnixNano() / 1000000,10)
+	if _, err := os.Stat("./testUploadImage/"); os.IsNotExist(err) {
 
-		} `json:"database"`
-
+		os.Mkdir("./testUploadImage/",os.ModePerm)
 	}
-	if r.Method == http.MethodPost{
-		file,header,err :=r.FormFile("uploadfile")
-		if err !=nil{
-			log.Println("uploading error",err)
-			http.Error(w,"error in uploading file",http.StatusInternalServerError)
-			return
+	imageUrl := c.Ctx.Input.Param(":imageUrl")
+	fmt.Println("insideeeeeee",imageUrl)
+	if len(imageUrl) != 0 {
+
+		response, e := http.Get(imageUrl)
+		if e != nil {
+			fmt.Println("error is goinggg")
+			log.Fatal(e)
 		}
-		if _, err := os.Stat("./testUploadImage/"); os.IsNotExist(err) {
 
-			os.Mkdir("./testUploadImage/",os.ModePerm)
+		defer response.Body.Close()
+
+		//open a file for writing
+		file, err := os.Create("./testUploadJson/"+"avatar"+msec+".jpg")
+		if err != nil {
+			fmt.Println("error ins1")
+			log.Fatal(err)
 		}
-		mimeType := header.Header.Get("Content-Type")
-		fmt.Println("typeeeeee",mimeType)
+		// Use io.Copy to just dump the response body to the file. This supports huge files
+		_, err = io.Copy(file, response.Body)
+		if err != nil {
+			fmt.Println("error ins2")
+			log.Fatal(err)
+		}
+		file.Close()
+		fmt.Println("Success!")
 
+	}else{
+		type Config struct {
+			Database struct {
+				Picture     string `json:"picture"`
 
-		msec := strconv.FormatInt(time.Now().UnixNano() / 1000000,10)
+			} `json:"database"`
 
-		//for processing json files
-		if mimeType =="application/octet-stream"{
-			fmt.Println("inside json")
-
-
-			if _, err := os.Stat("./testUploadJson/"); os.IsNotExist(err) {
-
-				os.Mkdir("./testUploadJson/",os.ModePerm)
-			}
-			jsonFile, err := os.OpenFile("./testUploadJson/"+header.Filename, os.O_WRONLY|os.O_CREATE, 0666)
-			if err != nil {
-				fmt.Println(err)
+		}
+		if r.Method == http.MethodPost{
+			file,header,err :=r.FormFile("uploadfile")
+			if err !=nil{
+				log.Println("uploading error",err)
+				http.Error(w,"error in uploading file",http.StatusInternalServerError)
 				return
 			}
-			io.Copy(jsonFile, file)
+
+			mimeType := header.Header.Get("Content-Type")
+			fmt.Println("typeeeeee",mimeType)
+
+
+
+
+			//for processing json files
+			if mimeType =="application/octet-stream"{
+				fmt.Println("inside json")
+
+
+				if _, err := os.Stat("./testUploadJson/"); os.IsNotExist(err) {
+
+					os.Mkdir("./testUploadJson/",os.ModePerm)
+				}
+				jsonFile, err := os.OpenFile("./testUploadJson/"+header.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+				io.Copy(jsonFile, file)
+				defer file.Close()
+
+				file, e := ioutil.ReadFile("./testUploadJson/"+header.Filename)
+				if e != nil {
+					fmt.Println("File error: %v\n", e)
+					os.Exit(1)
+				}
+				fmt.Println("%s\n", string(file))
+
+
+				var config Config
+				configFile, err := os.Open("./testUploadJson/"+header.Filename)
+				defer configFile.Close()
+				if err != nil {
+					fmt.Println(err.Error())
+				}
+
+				jsonParser := json.NewDecoder(configFile)
+				jsonParser.Decode(&config)
+
+
+				fmt.Println("file we got is ",config.Database.Picture)
+				unbased, err := base64.StdEncoding.DecodeString(config.Database.Picture)
+				if err != nil {
+					fmt.Println("error",err)
+					panic("Cannot decode b64")
+				}
+
+				r := bytes.NewReader(unbased)
+				im, err := png.Decode(r)
+				if err != nil {
+					fmt.Println("error",err)
+					panic("Bad png")
+				}
+
+				f, err := os.OpenFile("./testUploadImage/"+"example"+msec+".png", os.O_WRONLY|os.O_CREATE, 0777)
+				if err != nil {
+					fmt.Println("error",err)
+					panic("Cannot open file")
+				}
+
+				png.Encode(f, im)
+
+
+			}
+
+			f, err := os.OpenFile("./testUploadImage/"+msec+header.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+			if err != nil {
+				fmt.Println("image 4 error",err)
+				return
+			}
+			fmt.Println("jst")
+
+			io.Copy(f, file)
 			defer file.Close()
-
-			file, e := ioutil.ReadFile("./testUploadJson/"+header.Filename)
-			if e != nil {
-				fmt.Println("File error: %v\n", e)
-				os.Exit(1)
-			}
-			fmt.Println("%s\n", string(file))
+			fmt.Fprintf(w,"file  uploaded")
+			//for creating thumbnails of uploading image
 
 
-			var config Config
-			configFile, err := os.Open("./testUploadJson/"+header.Filename)
-			defer configFile.Close()
-			if err != nil {
-				fmt.Println(err.Error())
-			}
+			if mimeType =="image/jpeg" {
+				thumbfile, err := os.Open("./testUploadImage/" + msec + header.Filename)
+				if err != nil {
+					fmt.Println("the error5")
+					log.Fatal(err)
+				}
 
-			jsonParser := json.NewDecoder(configFile)
-			jsonParser.Decode(&config)
+				// decode jpeg into image.Image
+				img, err := jpeg.Decode(thumbfile)
+				if err != nil {
+					fmt.Println("the error2")
+					log.Fatal(err)
 
+				}
+				thumbfile.Close()
 
-			fmt.Println("file we got is ",config.Database.Picture)
-			unbased, err := base64.StdEncoding.DecodeString(config.Database.Picture)
-			if err != nil {
-				fmt.Println("error",err)
-				panic("Cannot decode b64")
-			}
+				// resize to width 100  and height 100 using Lanczos resampling
+				// and preserve aspect ratio
+				m := resize.Resize(100, 100, img, resize.Lanczos3)
 
-			r := bytes.NewReader(unbased)
-			im, err := png.Decode(r)
-			if err != nil {
-				fmt.Println("error",err)
-				panic("Bad png")
-			}
+				out, err := os.Create("./testUploadImage/" + "thumb" + msec + header.Filename)
+				if err != nil {
+					fmt.Println("the error1")
+					log.Fatal(err)
+				}
+				defer out.Close()
 
-			f, err := os.OpenFile("./testUploadImage/"+"example"+msec+".png", os.O_WRONLY|os.O_CREATE, 0777)
-			if err != nil {
-				fmt.Println("error",err)
-				panic("Cannot open file")
-			}
+				// write new image to file
+				jpeg.Encode(out, m, nil)
+			}else if mimeType =="image/png"{
+				thumbfile, err := os.Open("./testUploadImage/" + msec + header.Filename)
+				if err != nil {
+					fmt.Println("the error5")
+					log.Fatal(err)
+				}
 
-			png.Encode(f, im)
+				// decode png into image.Image
+				img, err := png.Decode(thumbfile)
+				if err != nil {
+					fmt.Println("the error2")
+					log.Fatal(err)
 
+				}
+				thumbfile.Close()
 
-		}
+				// resize to width 100  and height 100 using Lanczos resampling
+				// and preserve aspect ratio
+				m := resize.Resize(100, 100, img, resize.Lanczos3)
 
-		f, err := os.OpenFile("./testUploadImage/"+msec+header.Filename, os.O_WRONLY|os.O_CREATE, 0666)
-		if err != nil {
-			fmt.Println("image 4 error",err)
-			return
-		}
-		fmt.Println("jst")
+				out, err := os.Create("./testUploadImage/" + "thumb" + msec + header.Filename)
+				if err != nil {
+					fmt.Println("the error1")
+					log.Fatal(err)
+				}
+				defer out.Close()
 
-		io.Copy(f, file)
-		defer file.Close()
-		fmt.Fprintf(w,"file  uploaded")
-	//for creating thumbnails of uploading image
+				// write new image to file
+				png.Encode(out, m)
+			}else if mimeType =="image/gif" {
+				thumbfile, err := os.Open("./testUploadImage/" + msec + header.Filename)
+				if err != nil {
+					fmt.Println("the error5")
+					log.Fatal(err)
+				}
 
+				// decode png into image.Image
+				img, err := gif.Decode(thumbfile)
+				if err != nil {
+					fmt.Println("the error2")
+					log.Fatal(err)
 
-		if mimeType =="image/jpeg" {
-			thumbfile, err := os.Open("./testUploadImage/" + msec + header.Filename)
-			if err != nil {
-				fmt.Println("the error5")
-				log.Fatal(err)
-			}
+				}
+				thumbfile.Close()
 
-			// decode jpeg into image.Image
-			img, err := jpeg.Decode(thumbfile)
-			if err != nil {
-				fmt.Println("the error2")
-				log.Fatal(err)
+				// resize to width 100  and height 100 using Lanczos resampling
+				// and preserve aspect ratio
+				m := resize.Resize(100, 100, img, resize.Lanczos3)
 
-			}
-			thumbfile.Close()
+				out, err := os.Create("./testUploadImage/" + "thumb" + msec + header.Filename)
+				if err != nil {
+					fmt.Println("the error1")
+					log.Fatal(err)
+				}
+				defer out.Close()
 
-			// resize to width 100  and height 100 using Lanczos resampling
-			// and preserve aspect ratio
-			m := resize.Resize(100, 100, img, resize.Lanczos3)
+				// write new image to file
+				gif.Encode(out, m,nil)
 
-			out, err := os.Create("./testUploadImage/" + "thumb" + msec + header.Filename)
-			if err != nil {
-				fmt.Println("the error1")
-				log.Fatal(err)
-			}
-			defer out.Close()
-
-			// write new image to file
-			jpeg.Encode(out, m, nil)
-		}else if mimeType =="image/png"{
-			thumbfile, err := os.Open("./testUploadImage/" + msec + header.Filename)
-			if err != nil {
-				fmt.Println("the error5")
-				log.Fatal(err)
-			}
-
-			// decode png into image.Image
-			img, err := png.Decode(thumbfile)
-			if err != nil {
-				fmt.Println("the error2")
-				log.Fatal(err)
 
 			}
-			thumbfile.Close()
-
-			// resize to width 100  and height 100 using Lanczos resampling
-			// and preserve aspect ratio
-			m := resize.Resize(100, 100, img, resize.Lanczos3)
-
-			out, err := os.Create("./testUploadImage/" + "thumb" + msec + header.Filename)
-			if err != nil {
-				fmt.Println("the error1")
-				log.Fatal(err)
-			}
-			defer out.Close()
-
-			// write new image to file
-			png.Encode(out, m)
-		}else if mimeType =="image/gif" {
-			thumbfile, err := os.Open("./testUploadImage/" + msec + header.Filename)
-			if err != nil {
-				fmt.Println("the error5")
-				log.Fatal(err)
-			}
-
-			// decode png into image.Image
-			img, err := gif.Decode(thumbfile)
-			if err != nil {
-				fmt.Println("the error2")
-				log.Fatal(err)
-
-			}
-			thumbfile.Close()
-
-			// resize to width 100  and height 100 using Lanczos resampling
-			// and preserve aspect ratio
-			m := resize.Resize(100, 100, img, resize.Lanczos3)
-
-			out, err := os.Create("./testUploadImage/" + "thumb" + msec + header.Filename)
-			if err != nil {
-				fmt.Println("the error1")
-				log.Fatal(err)
-			}
-			defer out.Close()
-
-			// write new image to file
-			gif.Encode(out, m,nil)
-
-
-		}
 		}else{
-		c.TplName ="templates/fileUpload.html"
+			c.TplName ="templates/fileUpload.html"
+		}
+
 	}
 
 
--- routers/routers.go
+++ routers/routers.go
@@ -3,10 +3,12 @@ package routers
 import (
 	"github.com/astaxie/beego"
 	"fileUpload/controllers"
-	"fmt"
+
 )
 
 func init(){
-	fmt.Println("jjjjj")
+
 	beego.Router("/fileUpload",&controllers.FileUploadController{},"*:FileUpload")
+	beego.Router("/fileUpload/{:imageUrl}",&controllers.FileUploadController{},"*:FileUpload")
+
 }
